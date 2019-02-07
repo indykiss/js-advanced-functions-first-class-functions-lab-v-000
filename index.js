16c09f9095bf2ae92b11115762aff76829db6079
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-07 16:57:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
 
-function returnFirstTwoDrivers(drivers) {
+
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(drivers) {
   return drivers.slice(0,2);
 }
