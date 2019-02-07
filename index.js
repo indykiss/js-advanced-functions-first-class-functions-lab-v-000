9b78ab3a4c63ffad0ce537f21a5f7fccf17806b7
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-07 16:58:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(-2);
 }
 
-const selectingDrivers = 
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
